892e2337e7f8d953a30003759cc2983b372e72da
deepdesai003 <did29@drexel.edu>
2020-03-27 19:01:46 -0400

    Create the Api using dot net core 3.1

---
+++ PhiladelphiaInmateLocator.WebApi/PhiladelphiaInmateLocator.WebApi/Models/Inmate.cs
@@ -0,0 +1,36 @@
+﻿namespace PhiladelphiaInmateLocator.WebApi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Inmate
+    {
+        /// <summary>
+        ///     Gets or sets the PrisonerID on this Inmate.
+        /// </summary>
+        public int Id { get; set; } = 0;
+
+        /// <summary>
+        ///     Gets or sets the FirstName on this Inmate.
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     Gets or sets the LastName on this Inmate.
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     Gets or sets the DOB of this Inmate.
+        /// </summary>
+        public DateTime DateOfBirth { get; set; } = default;
+
+        /// <summary>
+        ///     Gets or sets the PrisonerID on this Inmate.
+        /// </summary>
+        public string Location { get; set; } = string.Empty;
+    }
+}
---
+++ PhiladelphiaInmateLocator.WebApi/PhiladelphiaInmateLocator.WebApi/Models/InmateContext.cs
@@ -0,0 +1,17 @@
+﻿namespace PhiladelphiaInmateLocator.WebApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class InmateContext :DbContext
+    {
+        public InmateContext(DbContextOptions<InmateContext> options):base(options)
+        {
+        }
+
+        public DbSet<Inmate> Inmates { get; set; }
+    }
+}
