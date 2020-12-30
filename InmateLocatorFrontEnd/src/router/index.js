5e4944ead34ff9ad3132cff6dde9eb635d6f06c0
deepdesai003 <deepdesai003@gmail.com>
2020-12-30 00:59:39 -0500

    * Remove Route to not found

--- InmateLocatorFrontEnd/src/router/index.js
+++ InmateLocatorFrontEnd/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import NotFound from '../components/error-pages/NotFound';
+//import NotFound from '../components/error-pages/NotFound';
 import SearchVue from '../components/Search/Search';
 import Admin from '../components/Search/Admin';
 
