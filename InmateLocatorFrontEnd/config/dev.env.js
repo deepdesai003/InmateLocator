07afa57a5c58cb49723dec1ebd3b374ab46c0a7c
deepdesai003 <deepdesai003@gmail.com>
2021-04-12 20:11:57 -0400

    Develop (#18)
    * remove unwanted
    
    * Feature/vue.js front end (#5)
    
    * * Create a front end application
    * Implement get inmate feature for annomynous users
    
    * * Clean up front end
    
    * Clean api
    
    * * Address issue with showing data once search is clicked
    
    * * Covert table to bootstrap div
    
    * stop tracking dev files
    
    * * Add Navbar
    
    * * Add abitily to search for Admins
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * * Final clean up
    
    * * fix directory in azure pipelines
    
    * Update azure-static-web-apps-orange-sand-010e63e0f.yml
    
    * Update azure-static-web-apps-orange-sand-010e63e0f.yml
    
    * Rename package-lock.json to package-lock_noUSE.json
    
    * Revert "* fix directory in azure pipelines"
    
    This reverts commit 51c2a4ad85562333d42ed447cfee87864ee8c523.
    
    * * fix packages
    
    * * fix package dependencies* fix packages
    
    * * Remove unwanted slot tags
    
    * * Only authorize certains calls
    
    Co-authored-by: Deep Desai <did29@drexel.edu>
    Co-authored-by: Azure Static Web Apps <opensource@microsoft.com>
    
    * Feature/vue.js front end (#8)
    
    * * Create a front end application
    * Implement get inmate feature for annomynous users
    
    * * Clean up front end
    
    * * Address issue with showing data once search is clicked
    
    * * Covert table to bootstrap div
    
    * stop tracking dev files
    
    * * Add Navbar
    
    * * Add abitily to search for Admins
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * * Final clean up
    
    * * fix directory in azure pipelines
    
    * Update azure-static-web-apps-orange-sand-010e63e0f.yml
    
    * Update azure-static-web-apps-orange-sand-010e63e0f.yml
    
    * Rename package-lock.json to package-lock_noUSE.json
    
    * Revert "* fix directory in azure pipelines"
    
    This reverts commit 51c2a4ad85562333d42ed447cfee87864ee8c523.
    
    * * fix packages
    
    * * fix package dependencies* fix packages
    
    * * Remove unwanted slot tags
    
    * * Only authorize certains calls
    
    * Update package.json
    
    * Update packages
    
    * Update .eslintrc.js
    
    * Remove Eslint rules
    
    * * Add slots back to slot in the tabs
    
    * * Fix file name
    
    * * Addresses issue with Inmate search
    
    * * Remove Route to not found
    
    * * Fix Dist Location
    
    * * Remove bad route
    
    * * Update production endpoint
    
    * * Add Home and Fix the Search button on Navbar
    
    Co-authored-by: Deep Desai <did29@drexel.edu>
    Co-authored-by: Azure Static Web Apps <opensource@microsoft.com>
    
    * * Fix all eslint issues (#9)
    
    * * Fix EsLInt errors
    
    * * Update franework (#17)
    
    Co-authored-by: Deep Desai <did29@drexel.edu>
    Co-authored-by: Azure Static Web Apps <opensource@microsoft.com>

--- InmateLocatorFrontEnd/config/dev.env.js
+++ InmateLocatorFrontEnd/config/dev.env.js
@@ -1,8 +1,10 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+/* eslint-disable no-undef */
+"use strict";
+const merge = require("webpack-merge");
+const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://localhost:44303/api/"'
-})
+  API_ENDPOINT:
+    '"https://philadelphiainmatelocatorwebapi-dev-as.azurewebsites.net/api/"'
+});
